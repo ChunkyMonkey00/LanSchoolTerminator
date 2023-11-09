17d02b21bd5ec4d95333bbd06050373831d933c9
ChunkyMonkey00 <148571454+ChunkyMonkey00@users.noreply.github.com>
2023-11-09 10:55:37 -0500

    Add files via upload

---
+++ launch.ps1
@@ -0,0 +1,1 @@
+powershell.exe -windowstyle hidden -file ".\wechillin.ps1"
---
+++ wechillin.ps1
@@ -0,0 +1,12 @@
+﻿#What's new 09/25/23
+#Powershell will only send an output if it succesfully closes the LSAIRCLIENT
+
+$ErrorActionPreference="SilentlyContinue"
+                                                                    
+Write-Output "To abort at any time, close powershell."
+while($true){
+
+    taskkill /im LSAirClient.exe /f 2>&1
+    taskkill /im LSAirClientUI.exe /f 2>&1
+
+}
